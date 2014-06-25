a4992729ce1b89e9aeee1cb2cdaf5dbf233e0a5e
albatrosary <fumio.sagawa@gmail.com>
2014-06-25 15:23:11 +0900

    Single-page Applciation

---
+++ js/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name html5jenterprise.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myAngularAppApp
+ */
+angular.module('html5jenterprise')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+'use strict';
 
+/**
+ * @ngdoc overview
+ * @name myAngularAppApp
+ * @description
+ * # myAngularAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('html5jenterprise', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/readme', {
+        templateUrl: 'views/readme.html',
+        controller: 'MainCtrl'
+      })
+      .when('/archive', {
+        templateUrl: 'views/archive.html',
+        controller: 'MainCtrl'
+      })
+      .when('/aboutus', {
+        templateUrl: 'views/aboutus.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
